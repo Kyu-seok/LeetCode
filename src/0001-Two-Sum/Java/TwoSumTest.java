098fa0d1ad1bf8f0dd30c4286ed1cd77b0ff2f7b
Kyu-seok <yeumkyuseok@gmail.com>
2022-09-03 23:26:59 +0800

    Implement Solution & Test code for LeetCode question 0001

--- src/0001-Two-Sum/Java/TwoSumTest.java
+++ src/0001-Two-Sum/Java/TwoSumTest.java
@@ -3,28 +3,28 @@ import static org.junit.Assert.*;
 
 public class TwoSumTest {
 
-  private TwoSum twoSum;
+    private TwoSum twoSum;
 
-  @Before
-  public void setup() {
-    twoSum = new TwoSum();
-  }
+    @Before
+    public void setup() {
+        twoSum = new TwoSum();
+    }
 
-  @Test
-  public void test1() {
-    int[] expected = {0, 1};
-    int[] inputNums = {2, 7, 11, 15};
-    assertArrayEquals(expected, twoSum.twoSum(inputNums, 9));
-  }
+    @Test
+    public void test1() {
+        int[] expected = { 0, 1 };
+        int[] inputNums = { 2, 7, 11, 15 };
+        assertArrayEquals(expected, twoSum.twoSum(inputNums, 9));
+    }
 
-  @Test
-  public void test2() {
-    assertEquals(2, 2);
-  }
+    @Test
+    public void test2() {
+        assertEquals(2, 2);
+    }
 
-  @Test
-  public void test3() {
-    assertEquals(3, 3);
-  }
+    @Test
+    public void test3() {
+        assertEquals(3, 3);
+    }
 
 }
