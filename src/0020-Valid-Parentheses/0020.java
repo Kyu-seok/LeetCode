f0b759f146534d6848ce6b144a4759eaad89a610
Kyu-seok <yeumkyuseok@gmail.com>
2023-01-18 23:25:07 +0900

    Implement Java Solution for LeetCode question 0020

---
+++ src/0020-Valid-Parentheses/0020.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char c = s.charAt(i);
+
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.empty()) {
+                    return false;
+                } else if (stack.peek() == '(' && c != ')') {
+                    return false;
+                } else if (stack.peek() == '[' && c != ']') {
+                    return false;
+                } else if (stack.peek() == '{' && c != '}') {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+
+        return stack.empty();
+    }
+}
