71ff9d1fd4e1a5ad80003ea2275aa77937b9a0d6
Kyu-seok <yeumkyuseok@gmail.com>
2023-03-10 16:08:52 +0900

    Implement Java Solution for LeetCode question 0055

---
+++ src/0055-Jump-Game/0055.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public boolean canJum(int[] nums) {
+        int maxPos = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > maxPos) {
+                return false;
+            }
+            maxPos = Math.max(maxPos, nums[i] + i);
+        }
+        return true;
+    }
+}
